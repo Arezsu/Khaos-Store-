34f84e79d1538d17f7c02f3ebd7b984e6d54cbe7
Arezsu <Arezsupriv@gmail.com>
2026-03-13 01:13:44 -0500

    Version final: Registro sin fecha de nacimiento, audio funcionando, espera 10s

--- khaos_store/wsgi.py
+++ khaos_store/wsgi.py
@@ -8,7 +8,9 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'khaos_store.settings')
+
 application = get_wsgi_application()
